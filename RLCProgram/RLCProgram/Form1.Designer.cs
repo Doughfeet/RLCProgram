412aa3ed8b53ec086c7b31617f50f0e485511a60
doughfeet <kennethblix@gmail.com>
2017-11-21 11:31:18 +0100

    Small steps... Voltage, current for all three

--- RLCProgram/RLCProgram/Form1.Designer.cs
+++ RLCProgram/RLCProgram/Form1.Designer.cs
@@ -28,16 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
             System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.chartControl = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.StartButton1 = new System.Windows.Forms.Button();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.Amp_NumericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.PhaseNumericUpDown2 = new System.Windows.Forms.NumericUpDown();
             this.label3 = new System.Windows.Forms.Label();
             this.R_NumericUpDown3 = new System.Windows.Forms.NumericUpDown();
             this.label4 = new System.Windows.Forms.Label();
@@ -45,31 +47,48 @@
             this.label5 = new System.Windows.Forms.Label();
             this.L_NumericUpDown5 = new System.Windows.Forms.NumericUpDown();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.FreqNumericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label8 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.chartControl)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Amp_NumericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PhaseNumericUpDown2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.R_NumericUpDown3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.C_NumericUpDown4)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.L_NumericUpDown5)).BeginInit();
             this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.FreqNumericUpDown1)).BeginInit();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // chartControl
             // 
-            chartArea2.Name = "ChartArea1";
-            this.chartControl.ChartAreas.Add(chartArea2);
-            legend2.Name = "Legend1";
-            this.chartControl.Legends.Add(legend2);
+            chartArea1.Name = "ChartArea1";
+            this.chartControl.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chartControl.Legends.Add(legend1);
             this.chartControl.Location = new System.Drawing.Point(12, 12);
             this.chartControl.Name = "chartControl";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.Legend = "Legend1";
+            series2.Name = "Series2";
             series3.ChartArea = "ChartArea1";
             series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
             series3.Legend = "Legend1";
-            series3.Name = "Series1";
+            series3.Name = "Series3";
             series4.ChartArea = "ChartArea1";
             series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
             series4.Legend = "Legend1";
-            series4.Name = "Series2";
+            series4.Name = "Series4";
+            this.chartControl.Series.Add(series1);
+            this.chartControl.Series.Add(series2);
             this.chartControl.Series.Add(series3);
             this.chartControl.Series.Add(series4);
             this.chartControl.Size = new System.Drawing.Size(1212, 807);
@@ -86,44 +105,45 @@
             this.StartButton1.UseVisualStyleBackColor = true;
             this.StartButton1.Click += new System.EventHandler(this.StartButton1_Click);
             // 
-            // numericUpDown1
+            // Amp_NumericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(218, 227);
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(205, 31);
-            this.numericUpDown1.TabIndex = 2;
-            this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.numericUpDown1.Value = new decimal(new int[] {
+            this.Amp_NumericUpDown1.Location = new System.Drawing.Point(218, 227);
+            this.Amp_NumericUpDown1.Name = "Amp_NumericUpDown1";
+            this.Amp_NumericUpDown1.Size = new System.Drawing.Size(205, 31);
+            this.Amp_NumericUpDown1.TabIndex = 2;
+            this.Amp_NumericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.Amp_NumericUpDown1.Value = new decimal(new int[] {
             1,
             0,
             0,
             0});
+            this.Amp_NumericUpDown1.ValueChanged += new System.EventHandler(this.Amp_NumericUpDown1_ValueChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(19, 232);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(107, 25);
+            this.label1.Size = new System.Drawing.Size(165, 25);
             this.label1.TabIndex = 3;
-            this.label1.Text = "Amplitude";
+            this.label1.Text = "Amplitude(V_o )";
             // 
-            // label2
+            // Label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(19, 287);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(60, 25);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Fase";
+            this.Label2.AutoSize = true;
+            this.Label2.Location = new System.Drawing.Point(24, 321);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(73, 25);
+            this.Label2.TabIndex = 3;
+            this.Label2.Text = "Phase";
             // 
-            // numericUpDown2
+            // PhaseNumericUpDown2
             // 
-            this.numericUpDown2.Location = new System.Drawing.Point(218, 281);
-            this.numericUpDown2.Name = "numericUpDown2";
-            this.numericUpDown2.Size = new System.Drawing.Size(205, 31);
-            this.numericUpDown2.TabIndex = 4;
-            this.numericUpDown2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.PhaseNumericUpDown2.Location = new System.Drawing.Point(218, 321);
+            this.PhaseNumericUpDown2.Name = "PhaseNumericUpDown2";
+            this.PhaseNumericUpDown2.Size = new System.Drawing.Size(205, 31);
+            this.PhaseNumericUpDown2.TabIndex = 4;
+            this.PhaseNumericUpDown2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // label3
             // 
@@ -158,9 +178,9 @@
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(19, 105);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(159, 25);
+            this.label4.Size = new System.Drawing.Size(171, 25);
             this.label4.TabIndex = 5;
-            this.label4.Text = "Capacitance(F)";
+            this.label4.Text = "Capacitance(uF)";
             // 
             // C_NumericUpDown4
             // 
@@ -201,28 +221,78 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.FreqNumericUpDown1);
             this.groupBox1.Controls.Add(this.label5);
-            this.groupBox1.Controls.Add(this.numericUpDown2);
+            this.groupBox1.Controls.Add(this.PhaseNumericUpDown2);
             this.groupBox1.Controls.Add(this.L_NumericUpDown5);
-            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.Label2);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.label3);
-            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Controls.Add(this.Amp_NumericUpDown1);
             this.groupBox1.Controls.Add(this.C_NumericUpDown4);
             this.groupBox1.Controls.Add(this.label4);
             this.groupBox1.Controls.Add(this.R_NumericUpDown3);
             this.groupBox1.Location = new System.Drawing.Point(1265, 26);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(432, 459);
+            this.groupBox1.Size = new System.Drawing.Size(432, 388);
             this.groupBox1.TabIndex = 9;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Parameters";
+            // 
+            // FreqNumericUpDown1
+            // 
+            this.FreqNumericUpDown1.Location = new System.Drawing.Point(223, 272);
+            this.FreqNumericUpDown1.Name = "FreqNumericUpDown1";
+            this.FreqNumericUpDown1.Size = new System.Drawing.Size(200, 31);
+            this.FreqNumericUpDown1.TabIndex = 7;
+            this.FreqNumericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.FreqNumericUpDown1.ValueChanged += new System.EventHandler(this.FreqNumericUpDown1_ValueChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(24, 279);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(115, 25);
+            this.label8.TabIndex = 3;
+            this.label8.Text = "Frequence";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Location = new System.Drawing.Point(1265, 471);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(579, 319);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Circut Info";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(218, 46);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(70, 25);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "label7";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(24, 47);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(110, 25);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Current_R";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1702, 1004);
+            this.ClientSize = new System.Drawing.Size(1884, 1004);
+            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.StartButton1);
             this.Controls.Add(this.chartControl);
@@ -230,13 +300,16 @@
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.chartControl)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Amp_NumericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PhaseNumericUpDown2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.R_NumericUpDown3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.C_NumericUpDown4)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.L_NumericUpDown5)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.FreqNumericUpDown1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -245,10 +318,10 @@
 
         private System.Windows.Forms.DataVisualization.Charting.Chart chartControl;
         private System.Windows.Forms.Button StartButton1;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown Amp_NumericUpDown1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label Label2;
+        private System.Windows.Forms.NumericUpDown PhaseNumericUpDown2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.NumericUpDown R_NumericUpDown3;
         private System.Windows.Forms.Label label4;
@@ -256,6 +329,11 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.NumericUpDown L_NumericUpDown5;
         private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.NumericUpDown FreqNumericUpDown1;
     }
 }
 
--- RLCProgram/RLCProgram/Form1.cs
+++ RLCProgram/RLCProgram/Form1.cs
@@ -15,10 +15,16 @@ namespace RLCProgram
 {
     public partial class Form1 : Form
     {
-        public double Count { get; set; } = 0;
+        public double Time { get; set; } = 0;
+
         public double Resistor { get; set; }
         public double Capacitor { get; set; }
         public double Inductor { get; set; }
+        public int Amplitude { get; set; }
+        public int Frequence { get; set; }
+        public double Phase { get; set; }
+        public double Current { get; set; }
+
 
 
 
@@ -27,17 +33,25 @@ namespace RLCProgram
         private double[] sinDataArray = new double[30];
         private double[] cosDataArray = new double[30];
 
+        private double[] CurrentResistiveArray = new double[30];
+        private double[] CurrentInductorArray = new double[30];
+        private double[] CurrentCapacitorArray = new double[30];
+
+
+
+
         public Form1()
         {
             InitializeComponent();
         }
-
         private void Form1_Load(object sender, EventArgs e)
         {
             Resistance();
             Capacitance();
             Inductance();
+            Freq(); 
         }
+
         private void Resistance()
         {
             R_NumericUpDown3.Increment = 1;
@@ -50,8 +64,8 @@ namespace RLCProgram
             C_NumericUpDown4.Increment = 0.1M;
             C_NumericUpDown4.DecimalPlaces = 1;
             C_NumericUpDown4.Minimum = 0M;
-            C_NumericUpDown4.Maximum = 10M;
-            C_NumericUpDown4.Value = 0.1M;
+            C_NumericUpDown4.Maximum = 500M;
+            C_NumericUpDown4.Value = 100M;
             //uF 10^-6
         }
         private void Inductance()
@@ -59,9 +73,13 @@ namespace RLCProgram
             L_NumericUpDown5.Increment = 0.1M;
             L_NumericUpDown5.DecimalPlaces = 1;
             L_NumericUpDown5.Minimum = 0M;
-            L_NumericUpDown5.Maximum = 10M;
-            L_NumericUpDown5.Value = 1;
-            //uH 10^-6
+            L_NumericUpDown5.Maximum = 200M;
+            L_NumericUpDown5.Value = 150M;
+        }
+
+        private void Freq()
+        {
+            FreqNumericUpDown1.Value = 60;
         }
 
 
@@ -69,15 +87,34 @@ namespace RLCProgram
         {
             this.Resistor = (double)R_NumericUpDown3.Value;
         }
-        private void C_NumericUpDown4_ValueChanged(object sender, EventArgs e)
-        {
-            this.Capacitor = (double)C_NumericUpDown4.Value;
-        }
         private void L_NumericUpDown5_ValueChanged(object sender, EventArgs e)
         {
             this.Inductor = (double)L_NumericUpDown5.Value;
         }
+        private void C_NumericUpDown4_ValueChanged(object sender, EventArgs e)
+        {
+            this.Capacitor = (double)C_NumericUpDown4.Value;
+        }
 
+        private void Amp_NumericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            Amplitude = (int)Amp_NumericUpDown1.Value;
+        }
+        private void FreqNumericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            Frequence = (int)FreqNumericUpDown1.Value;
+        }
+
+
+
+        private double InductiveReactance()
+        {
+            return 2 * Math.PI * Frequence * Inductor;
+        }
+        private double CapacitiveReactance()
+        {
+            return 1 / (2 * Math.PI * Frequence * Capacitor * Math.Pow(10, -6));
+        }
 
 
 
@@ -88,46 +125,79 @@ namespace RLCProgram
 
             while (true)
             {
-
-                sinDataArray[sinDataArray.Length - 1] = Math.Sin(Count);
+                sinDataArray[sinDataArray.Length - 1] = Amplitude * Math.Sin(2 * Math.PI * Time);
                 Array.Copy(sinDataArray, 1, sinDataArray, 0, sinDataArray.Length - 1);
 
-                cosDataArray[cosDataArray.Length - 1] = Math.Cos(2 * Math.PI * Count);
-                Array.Copy(cosDataArray, 1, cosDataArray, 0, cosDataArray.Length - 1);
+                
+                //Purely RESISIVE circut, is in phase with the applied voltage
+                CurrentResistiveArray[CurrentResistiveArray.Length - 1] = (Amplitude/Resistor) * Math.Sin(2 * Math.PI * Time);
+                Array.Copy(CurrentResistiveArray, 1, CurrentResistiveArray, 0, CurrentResistiveArray.Length - 1);
+
+
+                //Purely INDUCTOR circut, is in phase with the applied voltage
+                CurrentInductorArray[CurrentInductorArray.Length - 1] = (Amplitude / Inductor) * Math.Sin(2 * Math.PI * Time + (Math.PI / 2)) ;
+                Array.Copy(CurrentInductorArray, 1, CurrentInductorArray, 0, CurrentInductorArray.Length - 1);
+
+
+                //Purely CAPACITOR circut, is in phase with the applied voltage
+                CurrentCapacitorArray[CurrentCapacitorArray.Length - 1] = (Amplitude / Capacitor) * Math.Sin(2 * Math.PI * Time - (Math.PI / 2));
+                Array.Copy(CurrentCapacitorArray, 1, CurrentCapacitorArray, 0, CurrentCapacitorArray.Length - 1);
 
 
                 if (chartControl.IsHandleCreated)
                 {
-                    this.Invoke((MethodInvoker)delegate { UpdateDataChart(); });
+                    this.Invoke((MethodInvoker)delegate { UpdateDataChart1(); });
                     this.Invoke((MethodInvoker)delegate { UpdateDataChart2(); });
+                    this.Invoke((MethodInvoker)delegate { UpdateDataChart3(); });
+                    this.Invoke((MethodInvoker)delegate { UpdateDataChart4(); });
                 }
-                Count = Count + 0.2;
+                Time = Time + 0.2;
                 Thread.Sleep(250);
             }
 
 
         }
 
-        private void UpdateDataChart()
+        private void UpdateDataChart1()
         {
             chartControl.Series["Series1"].Points.Clear();
-
+            label7.Text = Math.Sin(400).ToString();
             for (int i = 0; i < sinDataArray.Length - 1; ++i)
             {
                 chartControl.Series["Series1"].Points.AddY(sinDataArray[i]);
             }
-
         }
 
         private void UpdateDataChart2()
         {
             chartControl.Series["Series2"].Points.Clear();
-
-            for (int i = 0; i < cosDataArray.Length - 1; ++i)
+            
+            for (int i = 0; i < CurrentResistiveArray.Length - 1; ++i)
             {
-                chartControl.Series["Series2"].Points.AddY(cosDataArray[i]);
+                label7.Text = CurrentResistiveArray[i].ToString();
+                chartControl.Series["Series2"].Points.AddY(CurrentResistiveArray[i]);
             }
         }
+
+        private void UpdateDataChart3()
+        {
+            chartControl.Series["Series3"].Points.Clear();
+
+            for (int i = 0; i < CurrentInductorArray.Length - 1; ++i)
+            {
+                chartControl.Series["Series3"].Points.AddY(CurrentInductorArray[i]);
+            }
+        }
+        private void UpdateDataChart4()
+        {
+            chartControl.Series["Series4"].Points.Clear();
+
+            for (int i = 0; i < CurrentCapacitorArray.Length - 1; ++i)
+            {
+                chartControl.Series["Series4"].Points.AddY(CurrentCapacitorArray[i]);
+            }
+        }
+
         private void StartButton1_Click(object sender, EventArgs e)
         {
             thread = new Thread(new ThreadStart(this.GetDataForSpline));
